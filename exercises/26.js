816c6bde8d2a82e710e025cd2fd5bb3cbb00e575
MaiCodes-exe <maise.b.costa@gmail.com>
2023-01-29 21:07:13 +0000

    26th ex

---
+++ exercises/26.js
@@ -0,0 +1,27 @@
+// Manipulating Complex Objects
+
+const myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man", 
+    "album": "BB",
+    "release_year": 1973, 
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man", 
+    "album": "BB",
+    "release_year": 1973, 
+        "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+  }
+];
