7ec9281c36939ad9aed09aebda62a930c34df48d
MaiCodes-exe <maise.b.costa@gmail.com>
2023-01-31 21:08:42 +0000

    40th ex

---
+++ exercises/40.js
@@ -0,0 +1,10 @@
+// Generate Random Fractions with JavaScript
+
+function randomFraction() {
+
+  // Only change code below this line
+
+  return Math.random(randomFraction);
+
+  // Only change code above this line
+}
